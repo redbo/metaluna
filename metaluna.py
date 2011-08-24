1893e2cb1a40fcfe753d916b6b2d7e2afeccc8b8
Mike Barton <mike@weirdlooking.com>
2011-08-24 12:09:14 -0700

    rename some stuff

--- metaluna.py
+++ metaluna.py
@@ -24,11 +24,11 @@ NBD_CLEAR_SOCK = 43780
 
 class BlockDevice(object):
     def __init__(self, size, device='/dev/nbd0'):
-        self.socketpair = socket.socketpair()
+        self.cli, self.srv = socket.socketpair()
         self.nbd = os.open(device, os.O_RDWR)
         ioctl(self.nbd, NBD_SET_BLKSIZE, BLOCK_SIZE)
         ioctl(self.nbd, NBD_SET_SIZE_BLOCKS, size / BLOCK_SIZE)
-        ioctl(self.nbd, NBD_SET_SOCK, self.socketpair[0].fileno())
+        ioctl(self.nbd, NBD_SET_SOCK, self.srv.fileno())
 
     def serve(self):
         thread = threading.Thread(target=lambda: ioctl(self.nbd, NBD_DO_IT))
@@ -37,26 +37,26 @@ class BlockDevice(object):
         try:
             while True:
                 magic, op, handle, offset, length = struct.unpack('!II8sQI',
-                                        self.socketpair[1].recv(REQUEST_LEN))
+                                        self.cli.recv(REQUEST_LEN))
                 errno = 0
                 if magic != NBD_REQUEST_MAGIC:
                     raise Exception('INVALID MAGIC')
                 elif op == NBD_CMD_READ:
                     data = self.read(offset, length)
                 elif op == NBD_CMD_WRITE:
-                    self.write(offset, self.socketpair[1].recv(length))
+                    self.write(offset, self.cli.recv(length))
                     data = ''
                 elif op == NBD_CMD_DISC:
                     raise Exception('DISCONNECT REQUEST')
                 else:
                     raise Exception('UNKNOWN TYPE %s' % op)
-                self.socketpair[1].sendall(struct.pack('!II8s',
+                self.cli.sendall(struct.pack('!II8s',
                             NBD_REPLY_MAGIC, errno, handle) + data)
         finally:
             ioctl(self.nbd, NBD_CLEAR_QUE)
             ioctl(self.nbd, NBD_CLEAR_SOCK)
-            self.socketpair[0].close()
-            self.socketpair[1].close()
+            self.cli.close()
+            self.srv.close()
 
     def read(self, offset, length):
         raise NotImplementedError()
